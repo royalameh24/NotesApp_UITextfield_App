da857fc06cdf17457f58fdcd36a3ad02169c3443
Roy Alameh <roy.alameh24@d155.org>
2022-08-31 16:57:27 -0500

    Added text fields and made label display results of both

--- NotesApp_UITextfield_App/ViewController.swift
+++ NotesApp_UITextfield_App/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    @IBOutlet weak var resultOutlet: UILabel!
+    @IBOutlet weak var ageFieldOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func clickAction(_ sender: UIButton) {
+        let name = textFieldOutlet.text!
+        let age = ageFieldOutlet.text!
+        resultOutlet.text = "\(name) is \(age) year(s) old"
+        
+    }
+    
 
 }
 
